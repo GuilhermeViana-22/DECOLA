6b0cc71ac7cd8630dfab6e7db68caf8c87a4194c
Guilherme Augusto dos Santos Viana <guilherme.gasv@policiacientifica.sp.gov.br>
2025-02-11 19:17:36 -0300

    ajustes

--- resources/views/components/curso/curso.blade.php
+++ resources/views/components/curso/curso.blade.php
@@ -32,13 +32,204 @@
                             <p class="price ms-2 mb-0 fs-4 text-success fw-bold">12x de <span class="fs-4">R$ 198,00</span></p>
                         </div>
 
-                        <!-- Contador de tempo -->
-                        <div class="d-flex justify-content-center mb-3">
-                            <div class="alert alert-warning w-100 p-3 d-flex justify-content-between align-items-center">
-                                <span class="fw-bold text-dark">Oferta válida pelas próximas:</span>
-                                <div id="countdown" class="fs-4 text-danger"></div>
+
+                        <!-- Chamada para ação -->
+
+                    </div>
+                    <a href=" {{ route('pages.checkout')}}" class="btn btn-primary w-100 py-3 text-white fs-5 fw-bold">Adquirir Agora</a>
+                </div>
+            </div>
+            <!-- Fim do Card -->
+
+
+               <!-- Card de Curso -->
+            <div class="col-12 col-sm-4 col-md-4 col-lg-4 mb-4">
+                <div class="curso-card card shadow-lg border-0 rounded-lg position-relative">
+                    <!-- Imagem -->
+                    <img src="{{ asset('extra-images/shop-gird-9.jpg') }}" class="card-img-top" alt="Curso Soft-skills">
+                    <div class="card-body text-center">
+                        <!-- Título -->
+                        <h5 class="card-title fs-5 fw-bold text-dark mb-2">Soft-skills</h5>
+
+                        <!-- Descrição -->
+                        <p class="card-text text-muted mb-3">Aprenda as 12 habilidades do futuro pela UNESCO para se destacar no mercado de trabalho.</p>
+
+                        <!-- Avaliação e Alunos -->
+                        <div class="d-flex justify-content-between mb-3">
+                            <div class="ratings">
+                                <span class="fas fa-star text-warning"></span>
+                                <span class="fas fa-star text-warning"></span>
+                                <span class="fas fa-star text-warning"></span>
+                                <span class="fas fa-star text-warning"></span>
+                                <span class="fas fa-star-half-alt text-warning"></span>
                             </div>
+                            <div class="students text-muted fs-6">Mais de 3.500 alunos</div>
                         </div>
+
+                        <!-- Parcelamento -->
+                        <div class="d-flex justify-content-between mb-3">
+                            <p class="price mb-0 fs-5 fw-bold text-secondary"><span class="fas fa-tag"></span> R$ 2.376,00 à vista</p>
+                            <p class="price ms-2 mb-0 fs-4 text-success fw-bold">12x de <span class="fs-4">R$ 198,00</span></p>
+                        </div>
+
+
+                        <!-- Chamada para ação -->
+
+                    </div>
+                    <a href=" {{ route('pages.checkout')}}" class="btn btn-primary w-100 py-3 text-white fs-5 fw-bold">Adquirir Agora</a>
+                </div>
+            </div>
+            <!-- Fim do Card -->
+
+
+               <!-- Card de Curso -->
+            <div class="col-12 col-sm-4 col-md-4 col-lg-4 mb-4">
+                <div class="curso-card card shadow-lg border-0 rounded-lg position-relative">
+                    <!-- Imagem -->
+                    <img src="{{ asset('extra-images/shop-gird-9.jpg') }}" class="card-img-top" alt="Curso Soft-skills">
+                    <div class="card-body text-center">
+                        <!-- Título -->
+                        <h5 class="card-title fs-5 fw-bold text-dark mb-2">Soft-skills</h5>
+
+                        <!-- Descrição -->
+                        <p class="card-text text-muted mb-3">Aprenda as 12 habilidades do futuro pela UNESCO para se destacar no mercado de trabalho.</p>
+
+                        <!-- Avaliação e Alunos -->
+                        <div class="d-flex justify-content-between mb-3">
+                            <div class="ratings">
+                                <span class="fas fa-star text-warning"></span>
+                                <span class="fas fa-star text-warning"></span>
+                                <span class="fas fa-star text-warning"></span>
+                                <span class="fas fa-star text-warning"></span>
+                                <span class="fas fa-star-half-alt text-warning"></span>
+                            </div>
+                            <div class="students text-muted fs-6">Mais de 3.500 alunos</div>
+                        </div>
+
+                        <!-- Parcelamento -->
+                        <div class="d-flex justify-content-between mb-3">
+                            <p class="price mb-0 fs-5 fw-bold text-secondary"><span class="fas fa-tag"></span> R$ 2.376,00 à vista</p>
+                            <p class="price ms-2 mb-0 fs-4 text-success fw-bold">12x de <span class="fs-4">R$ 198,00</span></p>
+                        </div>
+
+
+                        <!-- Chamada para ação -->
+
+                    </div>
+                    <a href=" {{ route('pages.checkout')}}" class="btn btn-primary w-100 py-3 text-white fs-5 fw-bold">Adquirir Agora</a>
+                </div>
+            </div>
+            <!-- Fim do Card -->
+
+
+               <!-- Card de Curso -->
+            <div class="col-12 col-sm-4 col-md-4 col-lg-4 mb-4">
+                <div class="curso-card card shadow-lg border-0 rounded-lg position-relative">
+                    <!-- Imagem -->
+                    <img src="{{ asset('extra-images/shop-gird-9.jpg') }}" class="card-img-top" alt="Curso Soft-skills">
+                    <div class="card-body text-center">
+                        <!-- Título -->
+                        <h5 class="card-title fs-5 fw-bold text-dark mb-2">Soft-skills</h5>
+
+                        <!-- Descrição -->
+                        <p class="card-text text-muted mb-3">Aprenda as 12 habilidades do futuro pela UNESCO para se destacar no mercado de trabalho.</p>
+
+                        <!-- Avaliação e Alunos -->
+                        <div class="d-flex justify-content-between mb-3">
+                            <div class="ratings">
+                                <span class="fas fa-star text-warning"></span>
+                                <span class="fas fa-star text-warning"></span>
+                                <span class="fas fa-star text-warning"></span>
+                                <span class="fas fa-star text-warning"></span>
+                                <span class="fas fa-star-half-alt text-warning"></span>
+                            </div>
+                            <div class="students text-muted fs-6">Mais de 3.500 alunos</div>
+                        </div>
+
+                        <!-- Parcelamento -->
+                        <div class="d-flex justify-content-between mb-3">
+                            <p class="price mb-0 fs-5 fw-bold text-secondary"><span class="fas fa-tag"></span> R$ 2.376,00 à vista</p>
+                            <p class="price ms-2 mb-0 fs-4 text-success fw-bold">12x de <span class="fs-4">R$ 198,00</span></p>
+                        </div>
+
+
+                        <!-- Chamada para ação -->
+
+                    </div>
+                    <a href=" {{ route('pages.checkout')}}" class="btn btn-primary w-100 py-3 text-white fs-5 fw-bold">Adquirir Agora</a>
+                </div>
+            </div>
+            <!-- Fim do Card -->
+
+               <!-- Card de Curso -->
+            <div class="col-12 col-sm-4 col-md-4 col-lg-4 mb-4">
+                <div class="curso-card card shadow-lg border-0 rounded-lg position-relative">
+                    <!-- Imagem -->
+                    <img src="{{ asset('extra-images/shop-gird-9.jpg') }}" class="card-img-top" alt="Curso Soft-skills">
+                    <div class="card-body text-center">
+                        <!-- Título -->
+                        <h5 class="card-title fs-5 fw-bold text-dark mb-2">Soft-skills</h5>
+
+                        <!-- Descrição -->
+                        <p class="card-text text-muted mb-3">Aprenda as 12 habilidades do futuro pela UNESCO para se destacar no mercado de trabalho.</p>
+
+                        <!-- Avaliação e Alunos -->
+                        <div class="d-flex justify-content-between mb-3">
+                            <div class="ratings">
+                                <span class="fas fa-star text-warning"></span>
+                                <span class="fas fa-star text-warning"></span>
+                                <span class="fas fa-star text-warning"></span>
+                                <span class="fas fa-star text-warning"></span>
+                                <span class="fas fa-star-half-alt text-warning"></span>
+                            </div>
+                            <div class="students text-muted fs-6">Mais de 3.500 alunos</div>
+                        </div>
+
+                        <!-- Parcelamento -->
+                        <div class="d-flex justify-content-between mb-3">
+                            <p class="price mb-0 fs-5 fw-bold text-secondary"><span class="fas fa-tag"></span> R$ 2.376,00 à vista</p>
+                            <p class="price ms-2 mb-0 fs-4 text-success fw-bold">12x de <span class="fs-4">R$ 198,00</span></p>
+                        </div>
+
+
+                        <!-- Chamada para ação -->
+
+                    </div>
+                    <a href=" {{ route('pages.checkout')}}" class="btn btn-primary w-100 py-3 text-white fs-5 fw-bold">Adquirir Agora</a>
+                </div>
+            </div>
+            <!-- Fim do Card -->
+
+               <!-- Card de Curso -->
+            <div class="col-12 col-sm-4 col-md-4 col-lg-4 mb-4">
+                <div class="curso-card card shadow-lg border-0 rounded-lg position-relative">
+                    <!-- Imagem -->
+                    <img src="{{ asset('extra-images/shop-gird-9.jpg') }}" class="card-img-top" alt="Curso Soft-skills">
+                    <div class="card-body text-center">
+                        <!-- Título -->
+                        <h5 class="card-title fs-5 fw-bold text-dark mb-2">Soft-skills</h5>
+
+                        <!-- Descrição -->
+                        <p class="card-text text-muted mb-3">Aprenda as 12 habilidades do futuro pela UNESCO para se destacar no mercado de trabalho.</p>
+
+                        <!-- Avaliação e Alunos -->
+                        <div class="d-flex justify-content-between mb-3">
+                            <div class="ratings">
+                                <span class="fas fa-star text-warning"></span>
+                                <span class="fas fa-star text-warning"></span>
+                                <span class="fas fa-star text-warning"></span>
+                                <span class="fas fa-star text-warning"></span>
+                                <span class="fas fa-star-half-alt text-warning"></span>
+                            </div>
+                            <div class="students text-muted fs-6">Mais de 3.500 alunos</div>
+                        </div>
+
+                        <!-- Parcelamento -->
+                        <div class="d-flex justify-content-between mb-3">
+                            <p class="price mb-0 fs-5 fw-bold text-secondary"><span class="fas fa-tag"></span> R$ 2.376,00 à vista</p>
+                            <p class="price ms-2 mb-0 fs-4 text-success fw-bold">12x de <span class="fs-4">R$ 198,00</span></p>
+                        </div>
+
 
                         <!-- Chamada para ação -->
 
